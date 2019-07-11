f6e05511893c6f46af1ea90f4b58789627781193
League Student <league@iMac.attlocal.net>
2019-07-10 18:32:24 -0700

    onto intro to enums

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -2,34 +2,70 @@ package _01_Custom_ArrayList;
 
 @SuppressWarnings("unchecked")
 
-public class ArrayList <T>{
-	
+public class ArrayList<T> {
+	private T[] arr;
+
 	public ArrayList() {
+		arr = (T[]) new Object[0];
 	}
-	
+
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+		return arr[loc];
 	}
-	
+
 	public void add(T val) {
-		
+		T[] newArr = (T[]) new Object[arr.length + 1];
+		newArr[newArr.length - 1] = val;
+		for (int i = 0; i < arr.length; i++) {
+			newArr[i] = arr[i];
+		}
+		arr = newArr;
 	}
-	
+
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		T[] insrtArr = (T[]) new Object[arr.length + 1];
+		for (int i = 0; i < insrtArr.length; i++) {
+			if (i < loc) {
+				insrtArr[i] = arr[i];
+			} else if (i > loc) {
+				insrtArr[i] = arr[i - 1];
+			} else {
+				insrtArr[i] = val;
+			}
+		}
+		arr=insrtArr;
 	}
-	
+
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		arr[loc] = val;
 	}
-	
+
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+		T[] rmveArr = (T[]) new  Object[arr.length-1];
+		for(int i =0; i<arr.length;i++) {
+			if(i<loc) {
+				rmveArr[i] = arr[i];
+			}
+			else if(i>loc) {
+				rmveArr[i-1] = arr[i];
+			}
+			else {
+				continue; 
+			}
+		}
+		arr = rmveArr; 
 	}
-	
+
 	public boolean contains(T val) {
-		
+		for(int i =0; i<arr.length;i++) {
+			if(arr[i].equals(val)) {
+				return true; 
+			}
+		}
 		return false;
+	}
+
+	public int size() {
+		return arr.length;
 	}
 }
--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -1,4 +1,4 @@
-package _03_Switch_Statement_Practice;
+ package _03_Switch_Statement_Practice;
 
 import javax.swing.JOptionPane;
 
@@ -6,12 +6,35 @@ public class CustomButtonOptionPanes {
 	public static void main(String[] args) {
 		String[] options = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
-		int input = JOptionPane.showOptionDialog(null, "Choose a day of the week", "Custom Buttons", 0, -1, null,
+		int input = JOptionPane.showOptionDialog(null, "Choose a day of the week to see all the days after it", "Custom Buttons", 0, -1, null,
 				options, 0);
 
 		String choice = options[input];
 		
 		//use a switch statement to do something cool for each option
+		switch(choice) {
+		case "Sunday":
+			System.out.println("Sunday");
 		
+		case "Monday":
+			System.out.println("Monday");
+	
+		case "Tuesday":
+			System.out.println("Tuesday");
+	
+		case "Wednesday":
+			System.out.println("Wednesday");
+		
+		case "Thursday":
+			System.out.println("Thursday");
+	
+		case "riday":
+			System.out.println("Friday");
+
+		case "Saturday":
+			System.out.println("Saturday");
+			break;
+		default : System.out.println("something went wrong");
+		}
 	}
 }
